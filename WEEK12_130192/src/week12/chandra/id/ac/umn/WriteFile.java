920a2e3bd6967e1b9b8835f4fe79ac654aeb4df6
user <user@CpfnajlalnfpakfF>
2025-12-02 18:49:36 +0700

    Write to myfile.txt

---
+++ WEEK12_130192/src/week12/chandra/id/ac/umn/WriteFile.java
@@ -0,0 +1,25 @@
+package week12.chandra.id.ac.umn;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class WriteFile {
+
+	public static void main(String[] args) {
+		try {
+			File Obj = new File("myfile.txt");
+			Scanner Reader = new Scanner(Obj);
+			while (Reader.hasNextLine()) {
+				String data = Reader.nextLine();
+				System.out.println(data);
+			}
+			Reader.close();
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("An error has occurred");
+			e.printStackTrace();
+		}
+	}
+}
