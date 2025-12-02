a929d6ab9035f6bbefa7d34a7489d436bf9d8e8b
user <user@CpfnajlalnfpakfF>
2025-12-02 18:49:57 +0700

    Delete myfile.txt

---
+++ WEEK12_130192/src/week12/chandra/id/ac/umn/DeleteFile.java
@@ -0,0 +1,17 @@
+package week12.chandra.id.ac.umn;
+
+import java.io.File;
+
+public class DeleteFile {
+
+	public static void main(String[] args) {
+		
+		File Obj = new File("myfile.txt");
+		if (Obj.delete()) {
+			System.out.println("the deleted file is: " +Obj.getName());
+		}
+		else {
+			System.out.println("Failed in deleting the file.");
+		}
+	}
+}
